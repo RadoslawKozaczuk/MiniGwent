97d5f76b13d29396e1284f919db2cd6b41028b78
Radoslaw <rakodoza@gmail.com>
2019-09-01 13:54:33 +0200

    Added Welcome Panel (not hooked up yet)
    - moved UI prefabs to dedicated folder

---
+++ Assets/Scripts/UI/WelcomePanelUI.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Core;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    class WelcomePanelUI : MonoBehaviour
+    {
+        public (PlayerIndicator position, PlayerControl control) TopPlayer = (PlayerIndicator.Top, PlayerControl.AI);
+        public (PlayerIndicator position, PlayerControl control) BotPlayer;
+
+        public void ChangeBotPlayer(int id) 
+            => BotPlayer = (PlayerIndicator.Bot, id == 0 ? PlayerControl.Human : PlayerControl.AI);
+
+        public void StartGame()
+        {
+
+        }
+    }
+}
